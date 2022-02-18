2128820e07f1032546cfe323e86062c24f96d0e0
AlexSpagettiFridge <SchiedAlex96@gmail.com>
2022-02-18 08:51:59 +0100

    oops

--- PackEntryCollection.cs
+++ PackEntryCollection.cs
@@ -1,22 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace XhunderUtil
 {
-    public struct PackEntryCollection<T>
+    public struct PackEntryCollection<T> : IEnumerable<KeyValuePair<string, T>>
     {
         private Dictionary<string, T> entries;
 
         public void Register(string pack, string subkey, T entry, bool allowOverwrite = true)
         {
-            if (entries == null)
-            {
-                entries = new Dictionary<string, T>();
-            }
             Register(GetCompleteString(pack, subkey), entry, allowOverwrite);
         }
 
         public void Register(string completeKey, T entry, bool allowOverwrite = true)
         {
+            if (entries == null)
+            {
+                entries = new Dictionary<string, T>();
+            }
             if (entries.ContainsKey(completeKey))
             {
                 if (!allowOverwrite)
@@ -25,11 +26,21 @@ namespace XhunderUtil
                 }
                 entries.Remove(completeKey);
             }
+            entries.Add(completeKey, entry);
+        }
+
+        public void Clear()
+        {
+            entries = new Dictionary<string, T>();
         }
 
         public T this[string key] => entries[key];
         public T this[string pack, string subkey] => entries[GetCompleteString(pack, subkey)];
 
         private string GetCompleteString(string pack, string subkey) => $"{pack}.{subkey}";
+
+        public IEnumerator<KeyValuePair<string, T>> GetEnumerator() => entries.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
